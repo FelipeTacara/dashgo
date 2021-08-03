375bf5ab8e7215ffdc09070471662b7e777453c5
Felipe Tacara <felipetacara@gmail.com>
2021-08-02 21:11:58 -0300

    react-query hooks

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,19 +1,32 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
-import Link from "next/link";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Link, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
+import NextLink from "next/link";
+import { useState } from "react";
 import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
+import { api } from "../../services/api";
 
-import { useUsers } from "../../services/hooks/useUsers";
+import { getUsers, useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
 
 export default function UserList() {
-    const { data, isLoading, isFetching, error } = useUsers()
+    const [page, setPage] = useState(1);
+    const { data, isLoading, isFetching, error } = useUsers(page)
 
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
     })
+
+    async function handlePrefetchUser(userId: string) {
+        await queryClient .prefetchQuery(['user', userId], async () => {
+            const response = await api.get(`users/${userId}`)
+
+            return response.data;
+        })
+    }
 
     return (
         <Box>
@@ -34,7 +47,7 @@ export default function UserList() {
                             { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />} 
                         </Heading>
 
-                        <Link href="/users/create" passHref>
+                        <NextLink href="/users/create" passHref>
                             <Button
                                 as="a"
                                 size="sm"
@@ -47,7 +60,7 @@ export default function UserList() {
                             >
                                 Criar novo
                             </Button>
-                        </Link>
+                        </NextLink>
                     </Flex>
 
                     {isLoading ? (
@@ -76,7 +89,7 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    {data.map(user => {
+                                    {data.users.map(user => {
                                         return (
                                             <Tr key={user.id}>
                                                 <Td px={["4", "4", "6"]}>
@@ -84,7 +97,9 @@ export default function UserList() {
                                                 </Td>
                                                 <Td>
                                                     <Box>
-                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Link onMouseEnter={() => handlePrefetchUser(user.id)}>
+                                                            <Text fontWeight="bold">{user.name}</Text>
+                                                        </Link>
                                                         <Text fontSize="small" color="gray.300">{user.email}</Text>
                                                     </Box>
                                                 </Td>
@@ -100,9 +115,9 @@ export default function UserList() {
                             </Table>
 
                             <Pagination 
-                                totalCountOfRegisters={200}
-                                currentPage={5}
-                                onPageChange={() => {}}                           
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}                           
                             />
                         </>
                     )}
@@ -110,4 +125,14 @@ export default function UserList() {
             </Flex>
         </Box>
     );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const {users, totalCount} = await getUsers(1)
+    
+    return {
+        props: {
+            users,
+        }
+    }
 }
--- src/services/miraje/index.ts
+++ src/services/miraje/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from 'miragejs'
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from 'miragejs'
 import faker from 'faker'
 
 
@@ -10,6 +10,10 @@ type User = {
 
 export function makeServer() {
     const server = createServer({
+        serializers: {
+            application: ActiveModelSerializer,
+        },
+
         models: {
             user: Model.extend<Partial<User>>({})
         },
@@ -50,10 +54,11 @@ export function makeServer() {
                 return new Response(
                     200,
                     { 'x-total-count': String(total)},
-                    { users }
+                    { users}
                 )
             });
 
+            this.get('users/:id')
             this.post('/users');
 
             this.namespace = '';
